5a614f64fbe0d1782c8c9a3a14c50335e2e2505a
Dev-Dan <adesanyaboluwatife29@gmail.com>
2022-05-09 23:54:59 +0100

    Almost done with the desktop UI

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function shop(){
     window.location.href = 'login.html'
 }
+let bar = document.getElementById('bar');
+let nav = document.getElementById('navbar');
+let close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', ()=>{
+        console.log('hello ');
+        nav.classList.add('active')
+    })
+}
+if(close){
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active')
+    })
+}
