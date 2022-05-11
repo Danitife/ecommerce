c53125bc8c0f82c1373e8f0612db0432f412d870
Dev-Dan <adesanyaboluwatife29@gmail.com>
2022-05-11 20:48:03 +0100

    Been able to validate a user

---
+++ dashboard/dashboard.js
@@ -0,0 +1,2 @@
+let current_user = JSON.parse(localStorage.getItem('user'))
+console.log(current_user);
--- login/login.js
+++ login/login.js
@@ -1,20 +1,27 @@
+let registered_user = JSON.parse(localStorage.getItem('user'))
+console.log(registered_user);
 
+let login = document.getElementById('login');
+if(login){
+    login.addEventListener('click', (e)=>{
+        e.preventDefault()
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
+        let message = document.getElementById('message');
+        let spinner = document.getElementById('sb')
 
-        let login = document.getElementById('login');
-        if(login){
-            login.addEventListener('click', (e)=>{
-                let registered_user = JSON.parse(localStorage.getItem('user'))
-                console.log(registered_user);
-                let email = document.getElementById('email'),value;
-                let password = document.getElementById('password').value;
-                let message = document.getElementById('message')
-                e.preventDefault()
-                if(email != registered_user[0].email && password != registered_user[0].password){
-                    message.innerHTML = "Invalid user information";
-                    console.log(registered_user[0].email);
-                    return;
-                }else{
-                    window.location.href = '/dashboard.html'
-                }
-            })
-        }
+        registered_user.forEach(element => {
+            if(email != element.email && password != element.password){
+                message.innerHTML = `<p style="background-color: rgb(177, 96, 96);">User Information Incorect</p>`
+                return;
+            }else{
+                spinner.setAttribute('style', 'display: block')
+                // setInterval(() => {
+                    let current_user = email
+                    console.log(current_user);
+                    // window.location.href = '../dashboard/dashboard.html'
+                // }, 3000);
+            }
+        });
+    })
+}
